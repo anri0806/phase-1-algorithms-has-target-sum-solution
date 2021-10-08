4c80a2965dfa923586d141cf6d3c7076539c366a
Anri <anri0806@hotmail.co.jp>
2021-10-08 17:11:35 -0400

    commit tree fe588b1b44c9b26a837d3e6cd23906a8f8d7ff41

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNum = new Set()
+  for(let number of array){
+    const foundIt = target - number
+    if(seenNum[foundIt]) return true;
+    seenNum[number] = true
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  time: one iteration, therefore O(n)
+  space: seenNum grows as input is larger, O(n)
 */
 
 /* 
   Add your pseudocode here
+  First, create empty object or set() to store seen num
+  Second, iterate num in array
+  Third, assign iterated num = target num - num we are looking for 
+  to variable
+  Fourth, if empty object or set() has above variable 
+  return true
+  ** Save the current number in the seen num variable
+  Fifth, if iterated until the end, no matching num
+  therefore return false
 */
 
 /*
@@ -24,6 +42,16 @@ if (require.main === module) {
 
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 3, 7], 3));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
 
   console.log("");
 
